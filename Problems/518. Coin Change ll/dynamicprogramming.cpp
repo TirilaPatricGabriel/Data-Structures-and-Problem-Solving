be7beac5bfa40804074a0a710be98d4b7c4d3c7f
Tîrîlă Patric-Gabriel <116957496+TirilaPatricGabriel@users.noreply.github.com>
2025-02-21 00:04:08 +0200

    518. Coin Change ll - dynamic programming - 100

---
+++ Problems/518. Coin Change ll/dynamicprogramming.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<uint64_t> dp(amount + 1, 0);
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+
+        return dp[amount] > INT_MAX ? 0 : dp[amount];
+    }
+};
+
+
+int main () {
+
+    return 0;
+}
