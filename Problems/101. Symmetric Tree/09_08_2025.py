de7576b0cb51ee78cabb4e905efaf3e6e5f64ac1
patric <patric@xcodesdevelopment.com>
2025-08-09 11:37:27 +0300

    101. Symmetric Tree

---
+++ Problems/101. Symmetric Tree/09_08_2025.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def checkSymmetry(self, leftNode: TreeNode, rightNode: TreeNode) -> bool:
+        if leftNode == None and rightNode == None:
+            return True
+
+        if leftNode == None or rightNode == None:
+            return False
+
+        return (leftNode.val == rightNode.val and self.checkSymmetry(leftNode.left, rightNode.right) and self.checkSymmetry(leftNode.right, rightNode.left))
