23ae4505c0daba0fd6deb1502400a81ea953f47a
Tîrîlă Patric-Gabriel <116957496+TirilaPatricGabriel@users.noreply.github.com>
2025-10-22 19:31:19 +0300

    5. Longest Palindromic Substring - dp

---
+++ Problems/5. Longest Palindromic Substring/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n <= 1:
+            return s
+
+        dp = [[False] * n for _ in range(n)]
+        
+        start = 0      
+        maxlen = 1     
+        
+        for i in range(n): 
+            dp[i][i] = True
+
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                start = i
+                maxlen = 2
+
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    maxlen = length
+                    start = i
+
+        return s[start:start + maxlen]
